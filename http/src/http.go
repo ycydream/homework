d83da06b549763893753978d0f2e9e651a384681
ycydream <765951811@qq.com>
2021-10-08 15:47:56 +0800

    'test'

--- http/src/http.go
+++ http/src/http.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"strings"
 )
 
@@ -11,6 +12,13 @@ func MyHandler(w http.ResponseWriter, r *http.Request) {
 	for k, v := range header {
 		//fmt.Println(k, v)
 		w.Header().Set(k, strings.Join(v, ""))
+	}
+	version := os.Getenv("VERSION")
+	fmt.Println(version)
+	if version == "" {
+		w.Header().Set("VERSION", "")
+	} else {
+		w.Header().Set("VERSION", version)
 	}
 	//fmt.Println("Header全部数据:", header)
 	fmt.Fprintf(w, "Hello World")
