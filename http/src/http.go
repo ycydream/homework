f4e58780ccf4d3e208a5f039aef0ee91c81135b9
ycydream <765951811@qq.com>
2021-10-08 14:58:10 +0800

    'test'

--- http/src/http.go
+++ http/src/http.go
@@ -3,9 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func MyHandler(w http.ResponseWriter, r *http.Request) {
+	header := r.Header
+	for k, v := range header {
+		//fmt.Println(k, v)
+		w.Header().Set(k, strings.Join(v, ""))
+	}
+	//fmt.Println("Header全部数据:", header)
 	fmt.Fprintf(w, "Hello World")
 }
 
